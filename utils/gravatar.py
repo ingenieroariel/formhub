d74e57cb6641272a6b1cd56b687fe7ee9cd02764
pld <peter@helioid.com>
2012-01-20 12:44:02 -0500

    local profile images

--- utils/gravatar.py
+++ utils/gravatar.py
@@ -2,9 +2,4 @@ import urllib, hashlib
 from django.core.urlresolvers import reverse
 
 def get_gravatar_img_link(user):
-    email = user.email
-    default = "https://formhub.org/static/images/formhub_avatar.png"
-    size = 40
-    gravatar_url = "https://secure.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
-    gravatar_url += urllib.urlencode({'d':default, 's':str(size)})
-    return gravatar_url
+    return "/static/images/formhub_avatar.png"
